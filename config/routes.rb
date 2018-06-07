b0b061c58a25d1af6ecc1241822d1998908510ed
Paulina Osiak <paulina.osiak@gmail.com>
2018-06-07 20:37:52 +0200

    Search funcionality is working

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
   get '/my_portfolio' => 'users#my_portfolio', as: 'my_portfolio'
+  get '/search_stocks' => 'stocks#search', as: 'search_stocks'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
