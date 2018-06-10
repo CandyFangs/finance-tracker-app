ef83e160dae60706319d9ad78fd30a91bf7878a5
Paulina Osiak <paulina.osiak@gmail.com>
2018-06-10 16:12:34 +0200

    using StockQuote gem fixed

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,12 +1,11 @@
 class Stock < ApplicationRecord
   
   def self.new_from_lookup(ticker_symbol)
-    begin 
+    begin
       looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
-      
     
-      price = strip_commas(looked_up_stock.l)
-      new(name: looked_up_stock.name, ticker: looked_up_stock.symbol, last_price: price)
+      price = looked_up_stock.latest_price
+      new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: price)
     rescue Exception => e 
       return nil
     end
