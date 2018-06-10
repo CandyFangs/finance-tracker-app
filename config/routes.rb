1a9912a7df54158ab0851ae39bad9fb69cccc6d6
Paulina Osiak <paulina.osiak@gmail.com>
2018-06-10 18:06:29 +0200

    Listing of stocks in my portfolio with functions added

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get '/my_portfolio' => 'users#my_portfolio', as: 'my_portfolio'
   get '/search_stocks' => 'stocks#search', as: 'search_stocks'
-  resources :user_stocks, only: [:create]
+  resources :user_stocks, only: [:create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
