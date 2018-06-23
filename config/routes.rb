e17084eba1b9cca6100c7561a9318c7c39dd25aa
Paulina Osiak <paulina.osiak@gmail.com>
2018-06-23 21:10:55 +0200

    Friendship model added

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get '/my_portfolio' => 'users#my_portfolio', as: 'my_portfolio'
   get '/search_stocks' => 'stocks#search', as: 'search_stocks'
+  get '/my_friends' => 'users#my_friends', as: 'my_friends'  
   resources :user_stocks, only: [:create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
