3d8db811a3adbe07ea6014cf7ac3b3753e04bd63
Paulina Osiak <paulina.osiak@gmail.com>
2018-07-03 19:53:25 +0200

    Fished finance tracker app

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   get '/my_portfolio' => 'users#my_portfolio', as: 'my_portfolio'
   get '/search_stocks' => 'stocks#search', as: 'search_stocks'
   get '/my_friends' => 'users#my_friends', as: 'my_friends'  
+  get '/search_friends' => 'users#search', as: 'search_friends'
+  post '/add_friend', to: "users#add_friend"
   resources :user_stocks, only: [:create, :destroy]
+  resources :users, only: [:show]
+  resources :friendships
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
